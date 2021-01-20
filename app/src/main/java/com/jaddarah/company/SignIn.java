97c378e87642cd3af31be19a417263d47e4ac745
Abdulsalam Abdu lkareem Ahmed <75985954+abdulsalam-alhammadi@users.noreply.github.com>
2021-01-20 19:10:11 +0300

    Rename Main Activity To SignIn #1

--- app/src/main/java/com/jaddarah/company/SignIn.java
+++ app/src/main/java/com/jaddarah/company/SignIn.java
@@ -4,15 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
 
         findViewById(R.id.btn_new_account).setOnClickListener(v -> {
             startActivity(new Intent(SignIn.this, CreateAccount.class));
